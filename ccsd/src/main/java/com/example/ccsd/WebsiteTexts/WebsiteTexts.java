dbcc11b2b289ee096e6e54e7d0af315c531e3832
zulikqmal <ikqmalzul@gamil.com>
2024-11-20 17:09:56 +0800

    WebsiteTexts

--- ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTexts.java
+++ ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTexts.java
@@ -1,5 +1,41 @@
 package com.example.ccsd.WebsiteTexts;
-
 public class WebsiteTexts {
-    
+
+    private String title;
+    private String content;
+    private String metaDescription;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getMetaDescription() {
+        return metaDescription;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setMetaDescription(String metaDescription) {
+        this.metaDescription = metaDescription;
+    }
+
+    public static void main(String[] args) {
+        WebsiteTexts txt = new WebsiteTexts();
+        txt.setTitle("Syafig");
+        txt.setContent("Hello"+"!");
+        txt.setMetaDescription("Lalalalala");
+        System.out.println("Title: " + txt.getTitle());
+        System.out.println("Content: " + txt.getContent());
+        System.out.println("Meta Description: " + txt.getMetaDescription());
+    }
 }
