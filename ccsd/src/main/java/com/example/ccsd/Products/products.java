637032388d54af797917b4c14c89b740d0bfc4e7
alimi <alimishazwan97@gmail.com>
2024-11-20 17:09:56 +0800

    Product

--- ccsd/src/main/java/com/example/ccsd/Products/products.java
+++ ccsd/src/main/java/com/example/ccsd/Products/products.java
@@ -1,5 +1,87 @@
 package com.example.ccsd.Products;
 
 public class products {
+    private String title;
+    private String string;
+    private String date; // dd/mm/yy
+    private String status;
+    private int productPlace;
+    private String tags;
+    private String descriptions;
+    private String websiteImage;
+    private String websiteGallery;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getProductPlace() {
+        return productPlace;
+    }
+
+    public void setProductPlace(int productPlace) {
+        this.productPlace = productPlace;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public String getWebsiteImage() {
+        return websiteImage;
+    }
+
+    public void setWebsiteImage(String websiteImage) {
+        this.websiteImage = websiteImage;
+    }
+
+    public String getWebsiteGallery() {
+        return websiteGallery;
+    }
+
+    public void setWebsiteGallery(String websiteGallery) {
+        this.websiteGallery = websiteGallery;
+    }
     
+
 }
