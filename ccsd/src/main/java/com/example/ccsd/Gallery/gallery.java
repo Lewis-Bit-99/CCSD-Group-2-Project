78624153f5971b7f6d3249ef29a387af49bcca61
dinAliah <ndina1943@gmail.com>
2024-11-20 17:17:26 +0800

    gallery

---
+++ ccsd/src/main/java/com/example/ccsd/Gallery/gallery.java
@@ -0,0 +1,25 @@
+package com.example.ccsd.Gallery;
+
+public class gallery {
+
+    private String image;
+    private String info;
+    
+
+    public String getImage(){
+        return image;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+    
+    public String getInfo(){
+        return info;
+    }
+
+    public void setInfo(String info){
+        this.info = info;
+    }
+
+    }
