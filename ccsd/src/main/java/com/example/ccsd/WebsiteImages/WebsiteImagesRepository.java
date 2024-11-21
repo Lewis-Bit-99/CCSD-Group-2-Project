4043b263c5f2ce4a02200d545b020424f1d7bf93
zulikqmal <ikqmalzul@gamil.com>
2024-11-21 15:03:10 +0800

    WebsiteTexts

--- ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImagesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ccsd.WebsiteImages;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WebsiteImages extends MongoRepository<WebsiteImages, String> {
-
-}
