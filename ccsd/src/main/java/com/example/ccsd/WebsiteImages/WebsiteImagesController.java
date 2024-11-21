0cb4d997d5e38951893aa86f185e2b8d9eb680cf
syahmiwxn <syahmiey73@gmail.com>
2024-11-21 17:31:13 +0800

    WebsiteImages

--- ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImagesController.java
+++ ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImagesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/WebSiteImages")
+@RequestMapping("/api/WebsiteImages")
 public class WebsiteImagesController {
 
     @Autowired
