992dd6e2a0bfc97cf397db06c1e23e9fd639b858
zulikqmal <ikqmalzul@gamil.com>
2024-11-23 22:30:52 +0800

    WebsiteTexts

--- ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTextsController.java
+++ ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTextsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -34,9 +35,17 @@ public class WebsiteTextsController {
     }
 
     @PostMapping
-    public WebsiteTexts addWebsiteTexts(@RequestBody WebsiteTexts texts){
-        return websiteTextsService.addWebsiteTexts(texts);
-    }
+public ResponseEntity<WebsiteTexts> addWebsiteText(
+    @RequestParam("postShortDescription") String postShortDescription,
+    @RequestParam("tag") String tag,
+    @RequestParam("title") String title,
+    @RequestParam("postSlug") String postSlug,
+    @RequestParam("status") String status
+) {
+    WebsiteTexts websiteText = new WebsiteTexts(title, status, tag, postShortDescription);
+    WebsiteTexts savedText = websiteTextsService.addWebsiteTexts(websiteText);
+    return ResponseEntity.ok(savedText);
+}
 
     @DeleteMapping
     public ResponseEntity<Void> deleteWebsiteTexts(@PathVariable String title){
