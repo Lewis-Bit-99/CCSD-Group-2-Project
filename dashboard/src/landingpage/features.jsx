b44b199a994d3c6d52bd7dc0e98c2151ac246a33
zulikqmal <ikqmalzul@gamil.com>
2024-11-25 00:04:37 +0800

    WebsiteTexts

--- dashboard/src/landingpage/features.jsx
+++ dashboard/src/landingpage/features.jsx
@@ -1,48 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const LandingPage = () => {
-  const [featuresData, setFeaturesData] = useState(null);
-
-  // Simulating data fetching
-  useEffect(() => {
-    const fetchData = [
-      { icon: "fa fa-check", title: "Feature 1", text: "This is feature 1" },
-      { icon: "fa fa-heart", title: "Feature 2", text: "This is feature 2" },
-      { icon: "fa fa-star", title: "Feature 3", text: "This is feature 3" },
-      { icon: "fa fa-cog", title: "Feature 4", text: "This is feature 4" },
-    ];
-    setTimeout(() => setFeaturesData(fetchData), 1000); // Simulate delay
-  }, []);
-
-  const Features = (props) => {
-    return (
-      <div id="features" className="text-center">
-        <div className="container">
-          <div className="col-md-10 col-md-offset-1 section-title">
-            <h2>Features</h2>
-          </div>
-          <div className="row">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                    <i className={d.icon}></i>
-                    <h3>{d.title}</h3>
-                    <p>{d.text}</p>
-                  </div>
-                ))
-              : "Loading..."}
-          </div>
+export const Features = (props) => {
+  return (
+    <div id="features" className="text-center">
+      <div className="container">
+        <div className="col-md-10 col-md-offset-1 section-title">
+          <h2>Features</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <h3>{d.title}</h3>
+                  <p>{d.text}</p>
+                </div>
+              ))
+            : "Loading..."}
         </div>
       </div>
-    );
-  };
-
-  return (
-    <div>
-      <h1>Welcome to the Landing Page</h1>
-      <Features data={featuresData} />
     </div>
   );
 };
-
-export default LandingPage;
