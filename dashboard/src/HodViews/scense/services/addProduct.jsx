8f635c4a2cca291de5f959f2b9e6f4f6d3ea587c
alimi <alimishazwan97@gmail.com>
2024-11-24 18:49:54 +0800

    Products

--- dashboard/src/HodViews/scense/services/addProduct.jsx
+++ dashboard/src/HodViews/scense/services/addProduct.jsx
@@ -20,6 +20,7 @@ import CK from '../../../Editor/ck';
 
 const AddProduct = () => {
     const theme = useTheme();
+    const navigate = useNavigate();
     const colors = tokens(theme.palette.mode);
     const [image, setImage] = useState(null);
     const [postShortDescription, setPostShortDescription] = useState(null);
@@ -28,8 +29,7 @@ const AddProduct = () => {
     const [title, setTitle] = useState(null);
     const [postSlug, setPostSlug] = useState(null);
     const [status, setStatus] = useState(null);
-    const navigate = useNavigate();
-    const editor = useRef(null);
+
 
     const handleChange = (event) => {
         setStatus(event.target.value);
@@ -47,11 +47,21 @@ const AddProduct = () => {
     const handleAddBlog = async (event) => {
         event.preventDefault(); // Prevent the default form submission behavior
       
+        const productData = {
+            tag,
+            title,
+            status,
+            place,
+            postShortDescription,
+            postSlug,
+            image: image ? URL.createObjectURL(image) : null, // If you are sending a base64 string or a file URL
+          };
+      
         try {
-            const success = await SaveItemsAdmin.addProductAdmin(place, postShortDescription, tag, title, postSlug, status, image);
+            const success = await SaveItemsAdmin.addProductAdmin(productData);
           
             if (success) {
-                navigate("/services");
+                navigate("/add-product");
             } else {
                 alert("Error Saving data");
             }
@@ -63,97 +73,93 @@ const AddProduct = () => {
 
     return (
         <Box>
-            <Header title="Add Product" subtitle="Please Fill All the Fields" />
-            <Box sx={{ display: 'flex', flexWrap: 'wrap' }} component="form" noValidate>
-                <TextField
-                    onChange={(e) => setTitle(e.target.value)}
-                    label="Enter Product Title"
-                    id="title"
-                    sx={{ m: 1, width: '30.5%' }}
-                    variant="filled"
-                />
-                <TextField
-                    onChange={(e) => setPostSlug(e.target.value)}
-                    label="Enter Product Slug"
-                    id="Slug"
-                    sx={{ m: 1, width: '30.5%' }}
-                    variant="filled"
-                />
-                <FormControl sx={{ m: 1, width: '30.5%' }} variant="filled">
-                    <FilledInput
-                        id='date'
-                        type='date'
-                    />
-                    <FormHelperText id="filled-dob-helper-text">Publish Date</FormHelperText>
-                </FormControl>
-                <FormControl sx={{ m: 1, width: '15.5%' }} variant="filled">
-                    <InputLabel id="status">Status</InputLabel>
-                    <Select
-                        labelId="status"
-                        id="status"
-                        value={status}
-                        label="status"
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={0}>Draft</MenuItem>
-                        <MenuItem value={1}>Publish</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl sx={{ m: 1, width: '15.5%' }} variant="filled">
-                    <InputLabel id="place">Product Place</InputLabel>
-                    <Select
-                        labelId="place"
-                        id="place"
-                        value={place}
-                        label="place"
-                        onChange={handleChangeplace}
-                    >
-                        {[...Array(12)].map((_, index) => (
-                            <MenuItem key={index + 1} value={index + 1}>{index + 1}</MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl sx={{ m: 1, width: '60%' }} variant="filled">
-                    <InputLabel htmlFor="filled-adornment-address">Tags</InputLabel>
-                    <FilledInput
-                        onChange={(e) => setTag(e.target.value)}
-                        id='tag'
-                        type='text'
-                    />
-                </FormControl>
-                <FormControl sx={{ m: 1, width: '93%' }} variant="filled">
-                    <InputLabel htmlFor="filled-adornment-short-description">Blog Short Description</InputLabel>
-                    <FilledInput
-                        onChange={(e) => setPostShortDescription(e.target.value)}
-                        id='short-description'
-                        type='text'
-                        multiline
-                        rows={3}
-                    />
-                </FormControl>
-                <FormControl sx={{ m: 1, width: '45%' }} variant="filled">
-                    <input
-                        accept="image/*"
-                        id="image-upload"
-                        type="file"
-                        onChange={handleImageChange}
-                    />
-                    <FormHelperText id="image-upload-helper-text">Blog Header Image</FormHelperText>
-                </FormControl>
-                <Box sx={{ m: 1, width: '93%', height: 600, color: theme.palette.mode === 'dark' ? 'black' : 'inherit' }}>
-                    <CK />
-                </Box>
-                <Button
-                    type="submit"
-                    sx={{ m: 1, width: '46%' }}
-                    color='success'
-                    variant="contained"
-                >
-                    Save
-                </Button>
-            </Box>
+          <Header title="Add Product" subtitle="Please Fill All the Fields" />
+    
+          {/* Form */}
+          <Box component="form" onSubmit={handleAddBlog} sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+            <TextField
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              label="Enter Product Title"
+              variant="filled"
+              sx={{ width: '30.5%' }}
+            />
+    
+            <FormControl sx={{ width: '15.5%' }} variant="filled">
+              <InputLabel id="status">Status</InputLabel>
+              <Select
+                labelId="status"
+                id="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <MenuItem value={0}>Draft</MenuItem>
+                <MenuItem value={1}>Publish</MenuItem>
+              </Select>
+            </FormControl>
+    
+            <FormControl sx={{ width: '60%' }} variant="filled">
+              <InputLabel htmlFor="tags">Tags</InputLabel>
+              <FilledInput
+                onChange={(e) => setTag(e.target.value)}
+                value={tag}
+                id="tag"
+                type="text"
+                
+              />
+            </FormControl>
+            <FormControl sx={{ m: 1, width: '60%' }} variant="filled">
+              <InputLabel htmlFor="short-description">Description</InputLabel>
+              <FilledInput
+                onChange={(e) => setPostShortDescription(e.target.value)}
+                value={postShortDescription}
+                id="postShortDescription"
+                type="text"
+                
+              />
+            </FormControl>
+    
+            <FormControl sx={{ width: '15.5%' }} variant="filled">
+              <InputLabel id="place">Text Place</InputLabel>
+              <Select
+                labelId="place"
+                id="place"
+                value={place}
+                onChange={(e) => setPlace(e.target.value)}
+              >
+                {[...Array(12).keys()].map(i => (
+                  <MenuItem key={i + 1} value={i + 1}>{i + 1}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+    
+
+            <FormControl sx={{ width: '45%' }} variant="filled">
+              <Input
+                accept="image/*"
+                id="image-upload"
+                type="file"
+                onChange={handleImageChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                      <SmartToyOutlinedIcon />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+    
+            {/* Save Button */}
+            <Button
+              type="submit"
+              sx={{ width: '46%', marginTop: '20px' }}
+              color="success"
+              variant="contained"
+            >
+              Save
+            </Button>
+          </Box>
         </Box>
-    );
+      );
 };
 
 export default AddProduct;
