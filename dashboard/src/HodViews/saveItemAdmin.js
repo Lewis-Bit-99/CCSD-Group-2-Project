82b0ae8bf474dd88b9f5c2dcc09d1fde92847b5c
Lewis-Bit-99 <lennoxlewis670@gmail.com>
2024-11-24 11:15:02 +0800

    Saved Item fix

--- dashboard/src/HodViews/saveItemAdmin.js
+++ dashboard/src/HodViews/saveItemAdmin.js
@@ -113,117 +113,12 @@ const SaveItemsAdmin = {
     const token = await localStorage.getItem('jwtToken');
     try {
       const formData = new FormData();
-      formData.append('categories', categories);
+      formData.append('place', parseInt(place));
       formData.append('tag', tag);
       formData.append('title', title);
       formData.append('status', status);
-      formData.append('place', place);
       formData.append('imageUrl', imageUrl);
-
-
-      if (image) {
-        formData.append('image', image); // Assuming 'image' is the key on the server to handle file uploads
-      }const response = await axios.post(
-  `${API_BASE_URL}/api/gallery`,
-  formData,
-  {
-    headers: {
-      'Content-Type': 'multipart/form-data', // Set content type to multipart/form-data for file uploads
-      Authorization: `Bearer ${token}`,
-    },
-  }
-);
-
-if (response.status === 200) {
-  return response.data;
-}
-} catch (error) {
-if (error.response) {
-  console.error('Server responded with an error:', error.response.data);
-} else if (error.request) {
-  console.error('No response received:', error.request);
-} else {
-  console.error('Error setting up the request:', error.message);
-}
-throw error;
-}
-},
-async addWebsiteImageAdmin (place, categories, tag, title, status, imageUrl) {
-const token = await localStorage.getItem('jwtToken');
-const username = await localStorage.getItem('userName');
-
-try {
-  const formData = new FormData();
-  formData.append('place', parseInt(place));
-  formData.append('tag', tag);
-  formData.append('title', title);
-  formData.append('status', status);
-  formData.append('imageUrl', imageUrl);
-  
-  const response = await axios.post(
-    `${API_BASE_URL}/api/WebsiteImages`,
-    formData,
-    {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${token}`,
-      }
-    }
-  );
-
-  if (response.status === 200) {
-    return response.data;
-  }
-} catch (error) {
-if (error.response) {
-  console.error('Server responded with an error:', error.response.data);
-} else if (error.request) {
-  console.error('No response received:', error.request);
-} else {
-  console.error('Error setting up the request:', error.message);
-}
-throw error;
-}
-},
-async addWebsiteTextAdmin( postShortDescription, tag, title, postSlug, status){
-const token = await localStorage.getItem('jwtToken');
-const username = await localStorage.getItem('userName');
-
-try {
-  const formData = new FormData();
-  formData.append('postShortDescription', postShortDescription);
-  formData.append('tag', tag);
-  formData.append('title', title);
-  formData.append('postSlug', postSlug);
-  formData.append('status', status);
-
-
-const response = await axios.post(
-  `${API_BASE_URL}/api/websiteTexts`,
-  formData,
-  {
-    headers: {
-      'Content-Type': 'multipart/form-data', // Set content type to multipart/form-data for file uploads
-      Authorization: `Bearer ${token}`,
-    },
-  }
-);
-
-if (response.status === 200) {
-  return response.data;
-}
-} catch (error) {
-if (error.response) {
-  console.error('Server responded with an error:', error.response.data);
-} else if (error.request) {
-  console.error('No response received:', error.request);
-} else {
-  console.error('Error setting up the request:', error.message);
-}
-throw error;
-}
-},
-
+      
       const response = await axios.post(
         `${API_BASE_URL}/api/WebsiteImages`,
         formData,
@@ -231,7 +126,7 @@ throw error;
           headers: {
             'Content-Type': 'multipart/form-data',
             Authorization: `Bearer ${token}`,
-          },
+          }
         }
       );
 
@@ -254,7 +149,7 @@ throw error;
         tag,
         title,
         postSlug,
-        status,
+      
       };
 
 
@@ -279,6 +174,6 @@ throw error;
       throw error;
     }
   }
-};
+}
 
 export default SaveItemsAdmin;
